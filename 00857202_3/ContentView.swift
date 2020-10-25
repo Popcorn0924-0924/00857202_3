4c877121f51dce3fbebff74499bda163f1832a24
jackson <jiechun.lai@gmail.com>
2020-10-25 14:56:07 +0800

    Initial Commit

---
+++ 00857202_3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  00857202_3
+//
+//  Created by User02 on 2020/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
